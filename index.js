d4771ca0ae0daac3e47dc44f76de5f8f1e03324c
claireeee <simplonco@debian>
2017-02-06 14:12:42 +0100

    ajout div commande

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var contenu = {
-    name: 'La bonne bouffe',
+    name: 'La grande bouffe',
     description: 'Venez goûter nos plats préparés avec amours et bienveillance. Notre chef Gertrude viendra probablement vous serrer la main. <br>Vous pourrez échanger avec cette passionnée de cuisine et repartir avec des conseils pour reproduire sa cuisine inimitable',
     images: [
         'https://media-cdn.tripadvisor.com/media/photo-s/02/1b/9a/bf/le-grande-bouffe.jpg',
@@ -15,19 +15,25 @@ var contenu = {
     carteGeo: 'Nous trouver',
     carte: [{
         name: 'Poulet aux morilles',
+        acheter:'commander',
         image: 'http://www.epicurien.be/img/recettes-cuisines/20130928_poulet_morilles-500.jpg',
         description: 'Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.',
-        price: '23 €'
+        price: '23 €',
+
     }, {
         name: 'Tagine aux pruneaux',
+        acheter:'commander',
         image: 'https://i1.wp.com/cotesoleils.fr/wp-content/uploads/2015/06/tajine-agneau-et-pruneaux-algc3a9rie1.jpg',
         description: 'Un des plus célèbres Tajines Marocains, servi avec une délicieuse viande de veau et des pruneaux issus de l\'agriculture biologique',
-        price: '18 €'
+        price: '18 €',
+
     }, {
         name: 'Salade de saison',
+        acheter:'commander',
         image: 'http://scrat.hellocoton.fr/img/guide/recette-ma-salade-tiede-de-chou-kale-et-lentilles-patates-douces-et-saumon-fume-18911644.jpg',
         description: 'Toujours à l\'heure, la salade de saison vous régalera avec ses légumes',
-        price: '14 €'
+        price: '14 €',
+
     }, ]
 }
 document.getElementById('titreSite').innerHTML = contenu.name;
@@ -48,9 +54,12 @@ function creationPlat() {
         var colonneContenu = document.createElement('div');
         var titre = document.createElement('h3');
         var descriptif = document.createElement('p');
+        var prix = document.createElement('div');
+        var commande = document.createElement('div');
+
 
         ligne.classList.add("row","col-xs-12", "col-sm-12");
-        colonneImage.classList.add("col-xs-9","col-sm-3");
+        colonneImage.classList.add("col-xs-12","col-sm-3");
         img.src = contenuCarte[i].image;
         img.classList.add("img-responsive","habillageImage");
         colonneVide.classList.add("col-xs-3","col-sm-3");
@@ -58,6 +67,11 @@ function creationPlat() {
         titre.innerHTML = contenuCarte[i].name;
         descriptif.innerHTML = contenuCarte[i].description;
         ligneVide.classList.add("taille");
+        prix.classList.add("designPrix","col-xs-6","col-sm-2");
+        prix.innerHTML = contenuCarte[i].price;
+        commande.classList.add("designPrix","col-xs-6","col-sm-3");
+        commande.innerHTML = contenuCarte[i].acheter;
+
 
 
         container2.appendChild(ligne);
@@ -68,6 +82,8 @@ function creationPlat() {
         colonneImage.appendChild(img);
         colonneContenu.appendChild(titre);
         colonneContenu.appendChild(descriptif);
+        colonneContenu.appendChild(prix);
+        colonneContenu.appendChild(commande);
 
     }
 };
